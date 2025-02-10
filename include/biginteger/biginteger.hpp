ab28358a80c30714726875c71819597cf6383de7
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-02-10 11:30:22 +0300

    Update CMake configuration to use C++20 and add numeric constants

--- include/biginteger/biginteger.hpp
+++ include/biginteger/biginteger.hpp
@@ -1,4 +1,30 @@
 #ifndef BIGINTEGER_HPP_goec3csb
 #define BIGINTEGER_HPP_goec3csb
 
+#include <concepts>
+#include <cstdint>
+
+namespace Numerics
+{
+
+template <typename T>
+concept UnsignedIntegral = std::unsigned_integral<T>;
+
+template <typename T>
+concept SignedIntegral = std::signed_integral<T>;
+
+namespace detail
+{
+
+struct NumericConstants
+{
+    static constexpr uint32_t BASE = 1000000000;
+    static constexpr int DECIMAL_DIGITS_PER_ELEMENT = 9;
+    static constexpr int MAX_POWER_OF_TEN = 999999999;
+};
+
+} // namespace detail
+
+} // namespace Numerics
+
 #endif // BIGINTEGER_HPP_goec3csb
--- tests/biginteger_test.cpp
+++ tests/biginteger_test.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 #include <biginteger/biginteger.hpp>
 
-TEST(BigIntegerTest, SampleTest) {
-    EXPECT_EQ("123", "123");
+TEST(NumericConstantsTest, ConstantsAreCorrect) {
+    using namespace Numerics::detail;
+
+    EXPECT_EQ(NumericConstants::BASE, 1000000000);
+    EXPECT_EQ(NumericConstants::DECIMAL_DIGITS_PER_ELEMENT, 9);
+    EXPECT_EQ(NumericConstants::MAX_POWER_OF_TEN, 999999999);
 }
