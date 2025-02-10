03541da3381bba252a59ee61fa617aef775db85b
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-02-10 10:32:06 +0300

    Remove BigInteger implementation and update example usage

--- examples/basic_usage.cpp
+++ examples/basic_usage.cpp
@@ -1,14 +1,7 @@
-#include <BigInteger/BigInteger.hpp>
-#include <iostream>
+#include <cstdio>
 
 int main()
 {
-    BigInteger a("123456789012345678901234567890");
-    BigInteger b("987654321098765432109876543210");
-
-    std::cout << "A = " << a.toString() << std::endl;
-    std::cout << "B = " << b.toString() << std::endl;
-    std::cout << "A + B = " << (a + b).toString() << std::endl;
-
+    (void)fprintf(stdout, "Hello, from the big integer library!\n");
     return 0;
 }
