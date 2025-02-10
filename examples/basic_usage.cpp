cf6f029523578fa85d1066e4a06f9d919d6c0704
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-02-10 09:47:38 +0300

    Configure clang format

--- examples/basic_usage.cpp
+++ examples/basic_usage.cpp
@@ -1,13 +1,14 @@
 #include <BigInteger/BigInteger.hpp>
 #include <iostream>
 
-int main() {
+int main()
+{
     BigInteger a("123456789012345678901234567890");
     BigInteger b("987654321098765432109876543210");
-    
+
     std::cout << "A = " << a.toString() << std::endl;
     std::cout << "B = " << b.toString() << std::endl;
     std::cout << "A + B = " << (a + b).toString() << std::endl;
-    
+
     return 0;
 }
