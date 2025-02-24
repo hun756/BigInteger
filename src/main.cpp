ae8eb2a48c457260bb3a9391f8cbba93aafc6c8a
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-02-24 10:39:13 +0300

    Refactor CMake configuration and restructure test setup

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    (void)fprintf(stdout, "Hello, from the Big Integer Library!\n");
+    return 0;
+}
--- test/biginteger_test.cpp
+++ test/biginteger_test.cpp
@@ -1,6 +1,6 @@
 #include <biginteger/biginteger.hpp>
 #include <gtest/gtest.h>
-
+#include <vector>
 
 TEST(NumericConstantsTest, ConstantsAreCorrect)
 {
