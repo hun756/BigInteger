090e9396bb4a0766183767b2894301b6b95c1bc6
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-03-19 17:05:33 +0300

    Add tests for is_prime_miller_rabin and modular_pow

--- test/arithmetic_operations_test.cpp
+++ test/arithmetic_operations_test.cpp
@@ -91,3 +91,93 @@ TEST(ArithmeticOperationsTest, ModularInverse)
     EXPECT_NE(inv_a, 0);
     EXPECT_EQ((a * inv_a) % m, 1);
 }
+
+TEST(ArithmeticOperationsTest, IsPrimeMillerRabin)
+{
+    using namespace Numerics::detail;
+
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(0));
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(1));
+    EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin(2));
+    EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin(3));
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(4));
+
+    std::vector<int> small_primes = {5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+    for (int prime : small_primes)
+    {
+        EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin(prime))
+            << "Failed on prime: " << prime;
+    }
+
+    std::vector<int> small_composites = {6,  8,  9,  10, 12, 14, 15, 16, 18,
+                                         20, 21, 22, 24, 25, 26, 27, 28};
+    for (int composite : small_composites)
+    {
+        EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(composite))
+            << "Failed on composite: " << composite;
+    }
+
+    std::vector<int64_t> larger_primes = {997,   1009,  1013,   1019,   10007,
+                                          10009, 10037, 100003, 100019, 100043};
+    for (int64_t prime : larger_primes)
+    {
+        EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin(prime))
+            << "Failed on prime: " << prime;
+    }
+
+    std::vector<int64_t> larger_composites = {999,   1001,   1003,   10001, 10003,
+                                              10005, 100001, 100009, 100015};
+    for (int64_t composite : larger_composites)
+    {
+        EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(composite))
+            << "Failed on composite: " << composite;
+    }
+
+    EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin<int64_t>(2147483647));
+    EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin<int64_t>(67280421310721));
+
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(-1));
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(-2));
+
+    EXPECT_TRUE(ArithmeticOperations::is_prime_miller_rabin(1000000007, 50));
+
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(561));
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(1105));
+    EXPECT_FALSE(ArithmeticOperations::is_prime_miller_rabin(1729));
+}
+
+TEST(ArithmeticOperationsTest, ModularPow)
+{
+    using namespace Numerics::detail;
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(2, 3, 5), 8 % 5);
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(3, 4, 7), 81 % 7);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(5, 0, 13), 1);
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(0, 0, 13), 1);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(0, 5, 13), 0);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(7, 1, 13), 7);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(12, 2, 13), 144 % 13);
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(12, 3, 13), (12 * 12 * 12) % 13);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(7, 20, 13), 3);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int64_t>(123456789LL, 2LL, 13LL), 1LL);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(2, 16, 17), 1);
+
+    EXPECT_EQ(ArithmeticOperations::modular_pow<int>(2, 100, 101), 1);
+
+    for (int base = 2; base <= 5; base++)
+    {
+        for (int exp = 2; exp <= 5; exp++)
+        {
+            int mod = 97;
+            int expected = static_cast<int>(std::fmod(std::pow(base, exp), mod));
+            EXPECT_EQ(ArithmeticOperations::modular_pow<int>(base, exp, mod), expected);
+        }
+    }
+}
